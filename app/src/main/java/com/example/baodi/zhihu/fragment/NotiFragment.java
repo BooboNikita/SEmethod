a66ff7763d11fe68352482f10ec33228a55b51f0
baodi <971969114@qq.com>
2018-12-11 10:13:03 +0800

    second

--- app/src/main/java/com/example/baodi/zhihu/fragment/NotiFragment.java
+++ app/src/main/java/com/example/baodi/zhihu/fragment/NotiFragment.java
@@ -20,7 +20,7 @@ import com.example.baodi.zhihu.R;
  */
 public class NotiFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    //    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
@@ -59,6 +59,7 @@ public class NotiFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,

20795383d41378ed136d263bb184b7335c0a796d
Raewyn <865081631@qq.com>
2018-12-18 10:06:06 +0800

    fourth commit

---
+++ app/src/main/java/com/example/baodi/zhihu/SomeClass/User.java
@@ -0,0 +1,18 @@
+package com.example.baodi.zhihu.SomeClass;
+
+import java.util.List;
+
+public class User {
+  public int useID;
+  public String nickname;
+  public String description;
+  public String profile;
+  public boolean sex;
+  public String placeInfo;
+  public String eduInfo;
+  public String vocation;
+  public String occupationInfo;
+  public String coverUrl;
+  public List<User> follow_user;
+
+}
